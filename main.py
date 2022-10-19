4b9725be34f9e9cdd35b40c15540b5e691555217
dankarger <karger.dan@gmail.com>
2022-10-20 01:23:32 +0300

    button

--- main.py
+++ main.py
@@ -2,31 +2,31 @@ from machine import Pin
 import time
 import random
 
-value = 0
+# Led:
+value = 1
 led = Pin(25, Pin.OUT)
 led_external = Pin(15, Pin.OUT)
 
-while True:
-    led(1)
-    led_external(b)
-    print('Value:{}'.format(value))
-    value = value + 1
-    time.sleep(0.5)
-    led_external(1)
-    time.sleep(0.25)
-    led_external(0)
-    time.sleep(0.25)
-    led(0)
-    led_external(1)
-    print('Led Off')
-    time.sleep(1)
-#
-#
-# import machine
-# import utime
-#
-# led_external = machine.Pin(15, machine.Pin.OUT)
-#
+# Button
+button = Pin(14, Pin.IN, Pin.PULL_DOWN)
+
 # while True:
-#     led_external.toggle()
-#     utime.sleep(5)
+#     led(1)
+#     led_external(b)
+#     print('Value:{}'.format(value))
+#     value = value + 1
+#     time.sleep(0.5)
+#     led_external(1)
+#     time.sleep(0.25)
+#     led_external(0)
+#     time.sleep(0.25)
+#     led(0)
+#     led_external(1)
+#     print('Led Off')
+#     time.sleep(1)
+
+while True:
+    if button.value() == 1:
+        print("You pressed the button! Value:{}".format(value) + ' times')
+        value = value + 1
+        time.sleep(0.3)
