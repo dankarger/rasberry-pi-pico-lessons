60828f5fc8d7bc5809d09422cde7e85cd8c2c7df
dankarger <karger.dan@gmail.com>
2022-10-20 02:17:50 +0300

    trafic lights - faster

--- main.py
+++ main.py
@@ -16,16 +16,16 @@ led_green = Pin(13, Pin.OUT)
 
 while True:
     led_red.value(1)
-    time.sleep(5)
+    time.sleep(0.2)
     led_amber.value(1)
-    time.sleep(2)
+    time.sleep(0.2)
     led_red.value(0)
     led_amber.value(0)
     led_green.value(1)
-    time.sleep(5)
+    time.sleep(0.5)
     led_green.value(0)
     led_amber.value(1)
-    time.sleep(5)
+    time.sleep(0.3)
     led_amber.value(0)
 #
 # while True:
