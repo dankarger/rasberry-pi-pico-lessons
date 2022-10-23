b7d274ae01ab9670e1fd495790bc392dfc6fc051
dankarger <karger.dan@gmail.com>
2022-10-24 00:12:15 +0300

    alarm -working

--- main.py
+++ main.py
@@ -2,22 +2,21 @@ import machine
 import time
 import utime
 
-#
-# sensor_pir = machine.Pin(28, machine.Pin.IN)
-# led = machine.Pin(15, machine.Pin.OUT)
+sensor_pir = machine.Pin(28, machine.Pin.IN)
+led = machine.Pin(15, machine.Pin.OUT)
 
-#
-# def pir_handler(pin):
-#     print("ALARM! Motion detected!")
-#     for i in range(50):
-#         led.value(1)
-#         utime.sleep_ms(100)
-#         led.value(0)
-#
-#
-# sensor_pir.irq(trigger=machine.Pin.IRQ_RISING, handler=pir_handler)
-#
-# while True:
-#     led.value(1)
-#     utime.sleep(5)
-#     led.value(0)
+
+def pir_handler(pin):
+    print("ALARM! Motion detected!")
+    for i in range(50):
+        led.toggle()
+        utime.sleep_ms(100)
+        # led.value(0)
+
+
+sensor_pir.irq(trigger=machine.Pin.IRQ_RISING, handler=pir_handler)
+
+while True:
+    led.toggle()
+    utime.sleep(5)
+    # led.value(0)
