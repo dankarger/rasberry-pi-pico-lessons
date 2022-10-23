fab3c5b6bc290d4f2a02c0ca37df946d23b3967c
dankarger <karger.dan@gmail.com>
2022-10-24 00:07:31 +0300

    alarm - problem

--- main.py
+++ main.py
@@ -1,127 +1,23 @@
-from machine import Pin
+import machine
 import time
 import utime
-import _thread
-import random
 
+#
+# sensor_pir = machine.Pin(28, machine.Pin.IN)
+# led = machine.Pin(15, machine.Pin.OUT)
 
-led_red = Pin(15, Pin.OUT)
-led_amber = Pin(14, Pin.OUT)
-led_green = Pin(13, Pin.OUT)
-buzzer = Pin(12, Pin.OUT)
-button = Pin(16, Pin.IN, Pin.PULL_DOWN)
-
-global button_pressed
-button_pressed = False
-
-
-def button_reader_thread():
-    global button_pressed
-    while True:
-        if button.value() == 1:
-            button_pressed = True
-
-
-_thread.start_new_thread(button_reader_thread, ())
-
-while True:
-    if button_pressed==True:
-        led_red.value(1)
-        for i in range(10):
-            buzzer.value(1)
-            utime.sleep(0.2)
-            buzzer.value(0)
-            utime.sleep(0.2)
-        global button_pressed
-        button_pressed = False
-    led_red.value(1)
-    utime.sleep(5)
-    led_amber.value(1)
-    utime.sleep(2)
-    led_red.value(0)
-    led_amber.value(0)
-    led_green.value(1)
-    utime.sleep(5)
-    led_green.value(0)
-    led_amber.value(1)
-    utime.sleep(5)
-    led_amber.value(0)
-
-
-
-
-
-
-
-# Led:
-# value = 1
-# led = Pin(25, Pin.OUT)
-# led_external = Pin(15, Pin.OUT)
-
-# Button
-# button = Pin(14, Pin.IN, Pin.PULL_DOWN)
-
+#
+# def pir_handler(pin):
+#     print("ALARM! Motion detected!")
+#     for i in range(50):
+#         led.value(1)
+#         utime.sleep_ms(100)
+#         led.value(0)
+#
+#
+# sensor_pir.irq(trigger=machine.Pin.IRQ_RISING, handler=pir_handler)
 #
 # while True:
-#     led_red.value(1)
-#     time.sleep(0.2)
-#     led_amber.value(1)
-#     time.sleep(0.2)
-#     led_red.value(0)
-#     led_amber.value(0)
-#     led_green.value(1)
-#     time.sleep(0.5)
-#     led_green.value(0)
-#     led_amber.value(1)
-#     time.sleep(0.3)
-#     led_amber.value(0)
-#
-#
-#
-#
-
-
-
-
-
-
-
-
-
-
-
-#
-# while True:
-#     led(1)
-#     led_external(0)
-#     print('Value:{}'.format(value))
-#     value = value + 1
-#     time.sleep(0.5)
-#     led_external(1)
-#     time.sleep(0.25)
-#     led_external(0)
-#     time.sleep(0.25)
-#     led(0)
-#     led_external(1)
-#     print('Led Off')
-#     time.sleep(1)
-#
-# while True:
-#     if button.value() == 1:
-#         print("You pressed the button! Value:{}".format(value) + ' times')
-#         value = value + 1
-#         time.sleep(0.3)
-
-#
-# while True:
-#     # if button.value() == 1:
-#     #     # print(button.value())
-#     #     print('t')
-#     #     led_external.value(0)
-#     #     time.sleep(2)
-#     led_external.value(1)
-#     led(1)
-#     time.sleep(1)
-#     print('2')
-#     led(0)
-#     led_external(0)
+#     led.value(1)
+#     utime.sleep(5)
+#     led.value(0)
