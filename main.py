9ab821fd870e4649bb58c757e0fb9ff6fd2e6b6f
dankarger <karger.dan@gmail.com>
2022-10-25 23:00:10 +0300

    Potentiometer  - lesson 16

--- main.py
+++ main.py
@@ -1,26 +1,11 @@
 import machine
 import utime
 
-sensor_pir = machine.Pin(28, machine.Pin.IN)
-sensor_pir2 = machine.Pin(22, machine.Pin.IN)
-led = machine.Pin(15, machine.Pin.OUT)
-buzzer = machine.Pin(14, machine.Pin.OUT)
-
-
-def pir_handler(pin):
-    if pin is sensor_pir:
-        print("ALARM! Motion detected in bedroom!")
-    elif pin is sensor_pir2:
-        print("ALARM! Motion detected in living room!")
-    for i in range(50):
-        led.toggle()
-        buzzer.toggle()
-        utime.sleep_ms(100)
-
-
-sensor_pir.irq(trigger=machine.Pin.IRQ_RISING, handler=pir_handler)
-sensor_pir2.irq(trigger=machine.Pin.IRQ_RISING, handler=pir_handler)
+potentiometer = machine.ADC(26)
+conversion_factor = 3.3 / (65535)
 
 while True:
-    led.toggle()
-    utime.sleep(5).sleep(5)
+    voltage = potentiometer.read_u16() * conversion_factor
+    print(voltage)
+    utime.sleep(2)
+    utime.sleep(5)
