5bfe4a1f930479cf04c9ecd2f7a3803e92f8c014
dankarger <karger.dan@gmail.com>
2022-10-27 00:25:01 +0300

    Fading an LED with PWM - with dimming off

--- main.py
+++ main.py
@@ -7,4 +7,7 @@ led.freq(1000)
 
 while True:
     print(potentiometer.read_u16())
-    led.duty_u16(potentiometer.read_u16())
+    if potentiometer.read_u16() > 770:
+        led.duty_u16(potentiometer.read_u16())
+    else:
+        led.duty_u16(0)
