ac10b5553b989223bc92096467519233dfdb548a
dankarger <karger.dan@gmail.com>
2022-10-26 23:59:26 +0300

    lesson 17 - Measuring_temperatures.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import machine
 import utime
 
-potentiometer = machine.ADC(26)
+sensor_temp = machine.ADC(4)
 conversion_factor = 3.3 / (65535)
 
 while True:
-    voltage = potentiometer.read_u16() * conversion_factor
-    print(voltage)
+    reading = sensor_temp.read_u16() * conversion_factor
+    temperature = 27 - (reading - 0.706)/0.001721
+    print(temperature)
     utime.sleep(2)
-    # utime.sleep(5)
