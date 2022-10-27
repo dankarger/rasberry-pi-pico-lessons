69443b31ff448d5a6e36a2314c39aaeee26698b6
dankarger <karger.dan@gmail.com>
2022-10-28 00:35:09 +0300

    file system = lesson 20 - loging tempretures

--- main.py
+++ main.py
@@ -5,15 +5,13 @@ sensor_temp = machine.ADC(4)
 conversion_factor = 3.3 / (65535)
 
 file = open("temp.txt", "w")
-string = open("number.txt")
-variable = string.read()
-string.close()
+file.close()
 
 while True:
     number = 1
     reading = sensor_temp.read_u16() * conversion_factor
     temperature = 27 - (reading - 0.706)/0.001721
-    file.write(variable + str(temperature) + "\n")
+    file.write(str(temperature) + "\n")
     number += 1
     file.flush()
     utime.sleep(1)
