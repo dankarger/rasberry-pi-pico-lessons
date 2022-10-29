bcdd641173ed789dbfef206fda1b2fb2d4fae1bd
dankarger <karger.dan@gmail.com>
2022-10-29 14:40:15 +0300

    lcd 1602 with temperature display

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import RGB1602
 import time
+import machine
 import math
+import write_text
+
+sensor_temp = machine.ADC(4)
+conversion_factor = 3.3 / (65535)
 
 colorR = 64
 colorG = 128
@@ -18,73 +23,29 @@ rgb7 = (80, 80, 145)  # 深蓝色
 rgb8 = (255, 0, 0)  # 红色
 rgb9 = (0, 255, 0)  # 青色
 
-
-# set the cursor to column 0, line 1
-
-
-def write_text(text):
-    lcd.setCursor(0, 0)
-    time.sleep(0.3)
-    for letter in text:
-        lcd.printout(letter)
-        time.sleep(0.3)
-
-
-def reset_lcd(seconds):
-    time.sleep(seconds)
-    lcd.clear()
-
+# space_invader = [0x4,0xe,0x1f,0x15,0x1f,0xa,0x1b,0x0]
+# lcd.display(0, space_invader)
 
 while True:
     lcd.setCursor(0, 0)
     lcd.setColorWhite()
-    write_text('Hello World!')
-    reset_lcd(1)
-    lcd.setRGB(rgb1[0], rgb1[1], rgb1[2])
+    write_text.write_text('Hello World!')
+    write_text.reset_lcd(1)
+    # lcd.setRGB(rgb1[0], rgb1[1], rgb1[2])
     lcd.setCursor(0, 0)
-    write_text("Nice to meet You")
-    lcd.setRGB(rgb3[0], rgb3[1], rgb3[2])
-    # reset_lcd(3)
+    reading = sensor_temp.read_u16() * conversion_factor
+    temperature = 27 - (reading - 0.706) / 0.001721
+    # print(temperature)
+    write_text.write_text(f"The Temp is:")
     lcd.setCursor(0, 1)
-    write_text(':)')
-    lcd.setRGB(rgb4[0], rgb4[1], rgb4[2])
+    write_text.write_text(f"- {str(temperature)[0:4]} C")
+    # lcd.setRGB(rgb3[0], rgb3[1], rgb3[2])
+    # reset_lcd(3)
+    lcd.setCursor(0, 0)
+    write_text.write_text(' - :)')
+    # lcd.setRGB(rgb4[0], rgb4[1], rgb4[2])
     time.sleep(2)
-    write_text(' Bye Bye!')
-    lcd.setRGB(rgb9[0], rgb9[1], rgb9[2])
-    reset_lcd(3)
+    write_text.write_text(' Bye Bye!')
+    # lcd.setRGB(rgb9[0], rgb9[1], rgb9[2])
+    write_text.reset_lcd(3)
     time.sleep(2)
-
-#
-#
-#
-# import RGB1602
-# import time
-# import math
-# import write_text
-#
-# colorR = 64
-# colorG = 128
-# colorB = 64
-#
-# lcd = RGB1602.RGB1602(16, 2)
-#
-# t = 0
-#
-# while True:
-#     r = int((abs(math.sin(3.14 * t / 180))) * 255);
-#     g = int((abs(math.sin(3.14 * (t + 60) / 180))) * 255);
-#     b = int((abs(math.sin(3.14 * (t + 120) / 180))) * 255);
-#     t = t + 3;
-#
-#     lcd.setRGB(r, g, b);
-#     # set the cursor to column 0, line 1
-#     lcd.setCursor(0, 0)
-#     # print the number of seconds since reset:
-#
-#     # print the number of seconds since reset:
-#     lcd.printout("1Waveshare")
-#     # write_text.write_text('Hello World')
-#     lcd.setCursor(0, 1)
-#
-#     # lcd.printout("Hello,World!")
-#     time.sleep(0.3)
--- write_text.py
+++ write_text.py
@@ -5,10 +5,10 @@ lcd = RGB1602.RGB1602(16, 2)
 
 def write_text(text):
     lcd.setCursor(0, 0)
-    time.sleep(0.3)
+    # time.sleep(interval)
     for letter in text:
         lcd.printout(letter)
-        time.sleep(0.3)
+        time.sleep(0.1)
 
 
 def reset_lcd(seconds):
