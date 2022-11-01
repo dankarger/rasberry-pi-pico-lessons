6e3964823610612845d223e97f9f572c16e5a014
dankarger <karger.dan@gmail.com>
2022-11-01 21:29:13 +0200

    led strip ws2812 with potentiometer to adjust speed

--- main.py
+++ main.py
@@ -3,10 +3,18 @@
 import array
 import time
 import rp2
+import machine
 from machine import Pin
+import utime
+import _thread
+import math
 
 # Configure the number of WS2812 LEDs.
 NUM_LEDS = 8
+
+potentiometer = machine.ADC(26)
+conversion_factor = 1000 / (65535)
+rate = 1000 - math.floor(potentiometer.read_u16() * conversion_factor)
 
 
 @rp2.asm_pio(sideset_init=rp2.PIO.OUT_LOW, out_shiftdir=rp2.PIO.SHIFT_LEFT, autopull=True, pull_thresh=24)
@@ -16,11 +24,11 @@ def ws2812():
     T3 = 3
     wrap_target()
     label("bitloop")
-    out(x, 1)               .side(0)    [T3 - 1]
-    jmp(not_x, "do_zero")   .side(1)    [T1 - 1]
-    jmp("bitloop")          .side(1)    [T2 - 1]
+    out(x, 1).side(0)[T3 - 1]
+    jmp(not_x, "do_zero").side(1)[T1 - 1]
+    jmp("bitloop").side(1)[T2 - 1]
     label("do_zero")
-    nop()                   .side(0)    [T2 - 1]
+    nop().side(0)[T2 - 1]
     wrap()
 
 
@@ -33,21 +41,38 @@ sm.active(1)
 # Display a pattern on the LEDs via an array of LED RGB values.
 ar = array.array("I", [0 for _ in range(NUM_LEDS)])
 
-# Cycle colours.
-for i in range(4 * NUM_LEDS):
-    for j in range(NUM_LEDS):
-        r = j * 100 // (NUM_LEDS - 1)
-        b = 100 - j * 100 // (NUM_LEDS - 1)
-        if j != i % NUM_LEDS:
-            r >>= 3
-            b >>= 3
-        ar[j] = r << 16 | b
-    sm.put(ar, 8)
-    time.sleep_ms(500)
 
-# Fade out.
-for i in range(24):
-    for j in range(NUM_LEDS):
-        ar[j] >>= 1
-    sm.put(ar, 8)
-    time.sleep_ms(500)
+def rate_reader():
+    global rate
+    while True:
+        # potentiometer.read_u16() > 770
+        voltage = potentiometer.read_u16() * conversion_factor
+        if voltage < 10:
+            rate = 2000
+        else:
+            rate = 1000 - math.ceil(voltage)
+        print(math.floor(voltage))
+        utime.sleep(2)
+
+
+_thread.start_new_thread(rate_reader, ())
+
+# Cycle colours.
+while True:
+    for i in range(4 * NUM_LEDS):
+        for j in range(NUM_LEDS):
+            r = j * 100 // (NUM_LEDS - 1)
+            b = 100 - j * 100 // (NUM_LEDS - 1)
+            if j!=i % NUM_LEDS:
+                r >>= 3
+                b >>= 3
+            ar[j] = r << 16 | b
+        sm.put(ar, 8)
+        time.sleep_ms(rate)
+
+    # Fade out.
+    for i in range(24):
+        for j in range(NUM_LEDS):
+            ar[j] >>= 1
+        sm.put(ar, 8)
+        time.sleep_ms(100)
